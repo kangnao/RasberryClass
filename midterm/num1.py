45a5c5249db5dea5a57a5dc9e3dceb4b6f40703f
kangnao <kangnao@naver.com>
2022-04-25 14:32:31 +0900

    홀수(odd)와 짝수의 합

---
+++ midterm/num1.py
@@ -0,0 +1,18 @@
+
+numbers=list([273,103,5,32,65,72,800,99])
+
+def odd(numbers):
+    for x in numbers:
+        if x %2 ==1:
+           print(odd)
+
+def even(numbers):
+    for x in numbers:
+        if x %2==0:
+            print(even)   
+            
+sumodd=sum(odd)
+sumeven=sum(even)
+
+print(sumodd)
+print(sumeven)
